669cc9cbce757320eb5752e0083b5709a39bf52b
Andrew Beatty <andrew.beatty@gmit.ie>
2025-02-20 21:18:10 +0000

    topic 04

--- code/Topic04-apis/messing2.py
+++
@@ -1,7 +0,0 @@
-# get planning applications
-# Author: Andrew Beatty
-import requests
-
-url = "https://opendata.arcgis.com/datasets/8f69dffe26324ba3acc653cf6cb5cf8b_0.geojson"
-list_of_planning = requests.get(url)
-print(list_of_planning.json()["features"][0])
